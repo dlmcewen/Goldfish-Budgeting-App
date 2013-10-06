59dc2119dc06fc823378a0bee8110cfbf4be4e1c
dlmcewen <davidmcewen@David-McEwens-MacBook.local>
2013-10-06 17:04:22 -0400

    Oct 6

--- src/com/goldfish/Home.java
+++ src/com/goldfish/Home.java
@@ -3,20 +3,26 @@ package com.goldfish;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
 public class Home extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_home);
+		setContentView(R.layout.activity_fullscreen);
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.home, menu);
 		return true;
+	}
+	
+	/** Called when the user clicks the Send button */
+	public void sendMessage(View view) {
+	    // Do something in response to button
 	}
 
 }
