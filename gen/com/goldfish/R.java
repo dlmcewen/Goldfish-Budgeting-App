43ed1332e00b30997e348bf0c8b4b030b06beeae
Dylan Daniel <dtdaniel@uga.edu>
2013-10-15 22:53:55 -0400

    pooperdoopers

--- gen/com/goldfish/R.java
+++ gen/com/goldfish/R.java
@@ -31,7 +31,7 @@ public final class R {
     }
     public static final class id {
         public static final int accounts=0x7f080009;
-        public static final int action_settings=0x7f080027;
+        public static final int action_settings=0x7f080029;
         public static final int amountTV=0x7f080015;
         public static final int budgetButton=0x7f080001;
         public static final int budgetTV=0x7f08000a;
@@ -58,7 +58,7 @@ public final class R {
         public static final int manual_entry_category_spinner=0x7f080019;
         public static final int manual_entry_done=0x7f080017;
         public static final int manual_entry_title=0x7f080014;
-        public static final int new_category_ET=0x7f080026;
+        public static final int new_category_ET=0x7f080028;
         public static final int progressBar1=0x7f080013;
         public static final int setDate=0x7f080021;
         public static final int setDays=0x7f08001f;
@@ -69,7 +69,9 @@ public final class R {
         public static final int textView1=0x7f080000;
         public static final int textView2=0x7f080005;
         public static final int textView3=0x7f080006;
+        public static final int trans1TV=0x7f080027;
         public static final int transactions=0x7f080008;
+        public static final int transactionsTitle=0x7f080026;
     }
     public static final class layout {
         public static final int activity_account_screen=0x7f030000;
--- src/com/goldfish/Transactions.java
+++ src/com/goldfish/Transactions.java
@@ -3,15 +3,24 @@ package com.goldfish;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class Transactions extends Activity {
 	
-	TransactionsData initialTransaction = new TransactionsData("Walmart", "10:15", "3/21/13", 20.37, true);
+	private TextView transaction1;
+	private TextView transaction2;
+	private TextView transaction3;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_transactions);
+		
+		TransactionsData transObj1 = new TransactionsData("Walmart", "10:15", "3/21/13", 20.37, true);
+		
+		transaction1 = (TextView)findViewById(R.id.trans1TV);
+		
+		transaction1.setText (transObj1.getStore() + "		" + transObj1.getAmount());
 	}
 
 	@Override
